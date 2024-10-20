8eb847640d93912c4e27b5d46182e9b3efb712d5
AMB2510 <anderson.barbosa8@fatec.sp.gov.br>
2024-10-20 12:59:15 -0300

    Atualização

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.androidXCore)
+    implementation(libs.kotlinStdLib)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
